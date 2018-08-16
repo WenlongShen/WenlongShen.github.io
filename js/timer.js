fcd27eaa5d12ed3935393060d35fe4b7c4628403
WenlongShen <shenwl1988@gmail.com>
2018-08-16 16:42:32 +0800

    js

--- js/timer.js
+++ js/timer.js
@@ -1,12 +1,26 @@
 function getRTime(){
-	var StartTime= new Date('2016/02/22 08:00:00');
 	var NowTime = new Date();
-	var t = NowTime.getTime() - StartTime.getTime();
+	// 相识
+	var StartTimeK = new Date('2016/02/22 08:00:00');
+	var k = NowTime.getTime() - StartTimeK.getTime();
+	var yk = NowTime.getFullYear() - StartTimeK.getFullYear() + 1;
+	var dk = Math.floor(k/1000/60/60/24);
+	// 领证
+	var StartTimeL = new Date('2017/11/27 14:00:00');
+	var l = NowTime.getTime() - StartTimeL.getTime();
+	var yl = NowTime.getFullYear() - StartTimeL.getFullYear() + 1;
+	var dl = Math.floor(l/1000/60/60/24);
+	// 婚礼
+	var StartTimeW = new Date('2018/03/16 12:00:00');
+	var w = NowTime.getTime() - StartTimeW.getTime();
+	var yw = NowTime.getFullYear() - StartTimeW.getFullYear() + 1;
+	var dw = Math.floor(w/1000/60/60/24);
 
-	var y = NowTime.getFullYear() - StartTime.getFullYear() + 1;
-	var d = Math.floor(t/1000/60/60/24);
-
-	document.getElementById("t_y").innerHTML = "<font size=3>"+ y + "年</font>";
-	document.getElementById("t_d").innerHTML = "<font size=3>"+ d + "天</font>";
+	document.getElementById("yk").innerHTML = "<font size=3>"+ yk + "年</font>";
+	document.getElementById("dk").innerHTML = "<font size=3>"+ dk + "天</font>";
+	document.getElementById("yl").innerHTML = "<font size=3>"+ yl + "年</font>";
+	document.getElementById("dl").innerHTML = "<font size=3>"+ dl + "天</font>";
+	document.getElementById("yw").innerHTML = "<font size=3>"+ yw + "年</font>";
+	document.getElementById("dw").innerHTML = "<font size=3>"+ dw + "天</font>";
 }
 setInterval(getRTime,1000);
